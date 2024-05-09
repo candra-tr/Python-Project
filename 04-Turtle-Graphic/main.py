acd3ff3c7669a2fc9bead045865ce5de003d6988
candra-tr <ctriatna@gmail.com>
2024-05-09 22:22:58 +0700

    random walk

--- 04-Turtle-Graphic/main.py
+++ 04-Turtle-Graphic/main.py
@@ -2,9 +2,12 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 
-num_sides = 5
 
-for _ in range(num_sides):
+def draw_shape(num_sides):
     angle = 360 / num_sides
-    tim.forward(100)
-    tim.right(angle)
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    draw_shape(shape_side_n)
