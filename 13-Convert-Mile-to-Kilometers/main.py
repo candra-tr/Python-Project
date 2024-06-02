5ea08ec2014f81d6fb12730216b2eadc02b68a98
candra-tr <ctriatna@gmail.com>
2024-06-02 21:51:54 +0700

    float to round

--- 13-Convert-Mile-to-Kilometers/main.py
+++ 13-Convert-Mile-to-Kilometers/main.py
@@ -1,9 +1,15 @@
 from tkinter import *
 
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilometer_result_label.config(text=f"{km}")
+
 window = Tk()
 window.title("Miles to Kilometer Converter")
+window.config(padx=20, pady=20)
 
-miles_input = Entry()
+miles_input = Entry(width=7)
 miles_input.grid(column =1, row=0)
 
 miles_label = Label(text="Miles")
@@ -18,7 +24,7 @@ kilometer_result_label.grid(column=1, row=1)
 kilometer_label = Label(text="Km")
 kilometer_label.grid(column=2, row=1)
 
-calculate_button = Button(text="Calculate")
+calculate_button = Button(text="Calculate", command=miles_to_km)
 calculate_button.grid(column=1, row=2)
 
 
